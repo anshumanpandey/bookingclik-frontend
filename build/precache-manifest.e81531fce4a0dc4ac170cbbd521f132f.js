f2d120c67aa21d8f7652cedb2be0c7d2065fa4d6
leo <leo_9_t@hotmail.com>
2020-04-11 12:00:01 -0400

    ADDED submodule for heroku

---
+++ build/precache-manifest.e81531fce4a0dc4ac170cbbd521f132f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f43628825bc7b437ce591e68cd41eba5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5523240a59e0f918eec5",
+    "url": "/static/css/main.970e724b.chunk.css"
+  },
+  {
+    "revision": "4aad83a85028c184c5b6",
+    "url": "/static/js/2.791758af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.791758af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5523240a59e0f918eec5",
+    "url": "/static/js/main.2cdf7266.chunk.js"
+  },
+  {
+    "revision": "5cf923f1681d9681a46b",
+    "url": "/static/js/runtime-main.454c014c.js"
+  }
+]);
