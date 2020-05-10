68d2146bbc401c8d53cc329470b8156af53dc941
root <leo_9_t@hotmail.com>
2020-05-10 08:09:33 -0400

    FIX on build

---
+++ build/precache-manifest.1eb46030de090e0dad68fc02af8991d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f6c2af0e9629ad106f0f7f53e4d0099",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f61d367f7151fe1e9e8",
+    "url": "/static/css/2.28e7212b.chunk.css"
+  },
+  {
+    "revision": "e455da2ba07e7254e277",
+    "url": "/static/css/main.0ab74ffc.chunk.css"
+  },
+  {
+    "revision": "0f61d367f7151fe1e9e8",
+    "url": "/static/js/2.14d4bc7d.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.14d4bc7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e455da2ba07e7254e277",
+    "url": "/static/js/main.a244ac30.chunk.js"
+  },
+  {
+    "revision": "cc365ccac2e173ce8292",
+    "url": "/static/js/runtime-main.f2fccf30.js"
+  }
+]);
