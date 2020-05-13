64a5b44407394f16cd10726115916534a0700f8d
root <leo_9_t@hotmail.com>
2020-05-13 14:00:59 -0400

    FIX on header

--- src/partials/Header.tsx
+++ src/partials/Header.tsx
@@ -5,18 +5,23 @@ import { Link } from 'react-router-dom';
 export const Header = () => {
 
     return (
-        <header style={{ backgroundColor: '#154a64' }} className="main-header dark-header fs-header sticky">
+        <header style={{ backgroundColor: '#154a64', display: 'flex', flexDirection: 'column' }} className="main-header dark-header fs-header sticky">
             <div className="header-inner" style={{ 
                 display: 'flex',
                 justifyContent: 'space-between',
+                alignItems: 'center',
+                flex: '1',
             }}>
-                <div className="logo-holder" style={{ margin: 'auto'}}>
+                <div className="logo-holder" style={{
+                    top: 'unset',
+                    height: 'unset'
+                }}>
                     <Link to="/">
                         <img style={{ height: 'auto',marginLeft: 'auto', marginRight: 'auto', width: '130px', marginTop: '-0.5rem' }} src={`${process.env.PUBLIC_URL}/images/logo.jpg`} alt="" />
                     </Link>
                 </div>
                 <div className="show-search-button"><i className="fa fa-search"></i> <span>Search</span></div>
-                <div className="show-reg-form modal-open"><i className="fa fa-sign-in"></i>Sign In</div>
+                <div style={{ position: 'unset' }} className="show-reg-form modal-open"><i className="fa fa-sign-in"></i>Sign In</div>
             </div>
         </header>
     );
