da9423daa5e8191055337bce8ce28c5e310cd020
root <leo_9_t@hotmail.com>
2020-05-13 10:43:54 -0400

    FIX on header logo

--- src/partials/Header.tsx
+++ src/partials/Header.tsx
@@ -5,9 +5,12 @@ export const Header = () => {
 
     return (
         <header style={{ backgroundColor: '#154a64' }} className="main-header dark-header fs-header sticky">
-            <div className="header-inner">
-                <div className="logo-holder">
-                    <a href="index.html"><img style={{ height: '80%'}} src="images/logo.jpg" alt="" /></a>
+            <div className="header-inner" style={{ 
+                display: 'flex',
+                justifyContent: 'space-between',
+            }}>
+                <div className="logo-holder" style={{ margin: 'auto'}}>
+                    <a href="index.html"><img style={{ height: 'auto',marginLeft: 'auto', marginRight: 'auto', width: '130px', marginTop: '-0.5rem' }} src="images/logo.jpg" alt="" /></a>
                 </div>
                 <div className="show-search-button"><i className="fa fa-search"></i> <span>Search</span></div>
                 <div className="show-reg-form modal-open"><i className="fa fa-sign-in"></i>Sign In</div>
