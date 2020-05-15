af075a55746b256a627c6fff2313035f7f7af187
root <leo_9_t@hotmail.com>
2020-05-15 14:38:47 -0400

    ADDED form error label

--- src/pages/brokerLogin/Login.tsx
+++ src/pages/brokerLogin/Login.tsx
@@ -5,6 +5,7 @@ import { Formik } from 'formik';
 import { logIn } from '../../crud/login.crud';
 import { dispatchGlobalState } from '../../state';
 import { useHistory } from 'react-router-dom';
+import ErrorLabel from '../../partials/ErrorLabel';
 
 interface MyFormValues {
     clientname: string
@@ -18,70 +19,76 @@ export const LoginPage: React.FC = () => {
 
     return (
         <>
-                <div className="main-register fl-wrap" style={{ borderRadius: 0, padding: 'unset', marginBottom: 'unset', boxShadow: 'inset' }}>
-                    <div className="custom-form" style={{ padding: '2rem' }}>
-                        <Formik
-                            initialValues={initialValues}
-                            validate={values => {
-                                const errors: { clientname?: string, password?: string } = {};
-                                if (!values.clientname) {
-                                    errors.clientname = 'Required';
-                                } else if (
-                                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.clientname)
-                                ) {
-                                    errors.clientname = 'Invalid email address';
-                                }
+            <div className="main-register fl-wrap" style={{ borderRadius: 0, padding: 'unset', marginBottom: 'unset', boxShadow: 'inset' }}>
+                <div className="custom-form" style={{ padding: '2rem' }}>
+                    <Formik
+                        initialValues={initialValues}
+                        validate={values => {
+                            const errors: { clientname?: string, password?: string } = {};
+                            if (!values.clientname) {
+                                errors.clientname = 'Required';
+                            } else if (
+                                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.clientname)
+                            ) {
+                                errors.clientname = 'Invalid email address';
+                            }
 
-                                if (!values.password) {
-                                    errors.password = 'Required';
-                                }
-                                return errors;
-                            }}
-                            onSubmit={(values, { setSubmitting }) => {
-                                post({ data: values })
-                                    .then(res => {
-                                        dispatchGlobalState({ type: 'token', state: res.data.token })
-                                        history.push({ pathname: '/profile' });
-                                    })
-                            }}
-                        >
-                            {({
-                                values,
-                                errors,
-                                touched,
-                                handleChange,
-                                handleBlur,
-                                handleSubmit,
-                                isSubmitting,
-                                /* and other goodies */
-                            }) => (
-                                    <form onSubmit={handleSubmit} name="registerform">
-                                        <label>{errors.clientname && touched.clientname && errors.clientname}</label>
-                                        <label>Email Address * </label>
-                                        <input
-                                            style={{ background: 'white', padding: '0.5rem' }}
-                                            name="clientname"
-                                            type="text"
-                                            onChange={handleChange}
-                                            onBlur={handleBlur}
-                                            value={values.clientname}
-                                        />
-                                        <label>{errors.password && touched.password && errors.password}</label>
-                                        <label >Password * </label>
-                                        <input
-                                            style={{ background: 'white', padding: '0.5rem' }}
-                                            name="password"
-                                            type="password"
-                                            onChange={handleChange}
-                                            onBlur={handleBlur}
-                                            value={values.password}
-                                        />
-                                        <button style={{ backgroundColor: '#154a64'}} type="submit" disabled={loading} className="log-submit-btn"><span>Log In</span></button>
-                                        <div className="clearfix"></div>
-                                    </form>
-                                )}
-                        </Formik>
-                    </div>
+                            if (!values.password) {
+                                errors.password = 'Required';
+                            }
+                            return errors;
+                        }}
+                        onSubmit={(values, { setSubmitting }) => {
+                            post({ data: values })
+                                .then(res => {
+                                    dispatchGlobalState({ type: 'token', state: res.data.token })
+                                    history.push({ pathname: '/profile' });
+                                })
+                        }}
+                    >
+                        {({
+                            values,
+                            errors,
+                            touched,
+                            handleChange,
+                            handleBlur,
+                            handleSubmit,
+                            isSubmitting,
+                            /* and other goodies */
+                        }) => (
+                                <form onSubmit={handleSubmit} name="registerform">
+                                    {error?.response?.data.error && (
+                                        <div style={{ margin: 0 }} className="notification reject fl-wrap">
+                                            {error?.response?.data.error}
+                                            <a className="notification-close" href="#"><i className="fa fa-times"></i></a>
+                                        </div>
+                                    )}
+                                    <ErrorLabel>{errors.clientname && touched.clientname && errors.clientname}</ErrorLabel>
+                                    <label>Email Address * </label>
+                                    <input
+                                        style={{ background: 'white', padding: '0.5rem' }}
+                                        name="clientname"
+                                        type="text"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.clientname}
+                                    />
+                                    <ErrorLabel>{errors.password && touched.password && errors.password}</ErrorLabel>
+                                    <label >Password * </label>
+                                    <input
+                                        style={{ background: 'white', padding: '0.5rem' }}
+                                        name="password"
+                                        type="password"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.password}
+                                    />
+                                    <button style={{ backgroundColor: '#154a64' }} type="submit" disabled={loading} className="log-submit-btn"><span>Log In</span></button>
+                                    <div className="clearfix"></div>
+                                </form>
+                            )}
+                    </Formik>
+                </div>
 
             </div>
         </>
---
+++ src/partials/ErrorLabel.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ErrorLabel: React.FC = ({ children }) => {
+
+    return (<label style={{ color: '#EA4D37'}}>{children}</label>);
+};
+
+export default ErrorLabel;
