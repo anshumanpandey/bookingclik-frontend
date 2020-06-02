fb779528215a10bfd5abed574fbb76b4242b9551
leo <leo_9_t@hotmail.com>
2020-06-02 15:02:55 -0400

    add missing file

--- src/partials/ListingItem.tsx
+++ src/partials/ListingItem.tsx
@@ -215,7 +215,7 @@ export const ListingItem: React.FC<ListingItemProps> = (props) => {
 
                         <div className="col-md-2">
                             <div className="geodir-category-location" style={{ marginTop: '0.5rem', marginBottom: '0.5rem', padding: 0, color: '#157f41' }} >
-                                <p style={{ color: '#157f41', paddingBottom: 0, textAlign: 'center' }}>Included for Free</p>
+                                <p style={{ color: '#157f41', paddingBottom: 0, textAlign: 'center' }}><i style={{ color: 'inherit' }} className="fas fa-check" />Included for Free</p>
                                 {props.vehicle.winter_tyres_included && <p style={{ textAlign: 'center', color: 'inherit', paddingBottom: 0 }}><i style={{ color: 'inherit' }} className="fas fa-check"></i> Winter Tyres Included</p>}
                                 {props.vehicle.snow_chains_included && <p style={{ textAlign: 'center', color: 'inherit', paddingBottom: 0 }}><i style={{ color: 'inherit' }} className="fas fa-check"></i> Snow Chains Included</p>}
                                 {props.vehicle.road_tax_included && <p style={{ textAlign: 'center', color: 'inherit', paddingBottom: 0 }}><i style={{ color: 'inherit' }} className="fas fa-check"></i>Road Tax Included</p>}
