3d513f23afbd6d421c3286e97b989306d6cd93c2
leo <leo_9_t@hotmail.com>
2020-06-08 12:43:29 -0400

    FIX on url

--- src/crud/email.crud.ts
+++ src/crud/email.crud.ts
@@ -3,6 +3,6 @@ import { AxiosRequestConfig } from "axios"
 export const sendEmail = (): AxiosRequestConfig => {
     return {
         method: 'POST',
-        url: `https://www.bookingclik.com/email.php`,
+        url: `https://www.carrentalclik.com/email.php`,
     }
 }
