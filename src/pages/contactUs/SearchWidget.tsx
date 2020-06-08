1e6ad477ed25e8c556c9fec4661d1c53f8c3ed12
leo <leo_9_t@hotmail.com>
2020-06-08 13:23:52 -0400

    ADDED success message

--- src/pages/contactUs/SearchWidget.tsx
+++ src/pages/contactUs/SearchWidget.tsx
@@ -5,6 +5,7 @@ import { CircularProgress, InputBase } from '@material-ui/core';
 import { sendEmail } from '../../crud/email.crud';
 import { useMediaQuery } from 'react-responsive'
 import qs from 'qs';
+import { dispatchGlobalState, useGlobalState } from '../../state';
 
 
 const normalUseAxios = makeUseAxios({
@@ -18,10 +19,8 @@ export const SearchWidget: React.FC = () => {
   const [description, setDescription] = useState('need');
   const isSm = useMediaQuery({ query: '(min-width: 768px)' })
 
-
   const [emailReq, send] = normalUseAxios<{ ip: string }>(sendEmail(), { manual: true })
-
-  console.log(emailReq)
+  const [success, setSuccess] = useGlobalState('success');
 
   return (
     <div className="main-search-input-wrap" style={{ maxWidth: '1096px' }}>
@@ -34,6 +33,13 @@ export const SearchWidget: React.FC = () => {
         borderRadius: '0.25rem',
       }}>
         <div className="main-search-input fl-wrap" style={{ display: 'flex', flexDirection: 'column', boxShadow: 'unset', padding: 0, background: 'unset', marginTop: 0, height: '100%', justifyContent: 'space-around' }}>
+          {success && (
+            <div className="notification success fl-wrap">
+              <p>{success}</p>
+              <a onClick={() => setSuccess(null)} className="notification-close" href="#">
+                <i className="fa fa-times"></i></a>
+            </div>
+          )}
           <div>
             <div className="row" style={{ display: 'flex', justifyContent: 'space-around', flexDirection: isSm ? 'unset' : 'column' }}>
               <div style={{ marginRight: '0.5rem', flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
@@ -75,18 +81,23 @@ export const SearchWidget: React.FC = () => {
                 />
               </div>
             </div>
-        </div>
-        <div>
-          <button className="main-search-button" onClick={() => axios.post(`https://carrentalclik.com/contact.php`,  qs.stringify({
-            to: email,
-            title: `${name}: ${subject}`,
-            body: description,
-          }))} style={{ position: 'relative', marginTop: '1rem', borderRadius: '0.25rem', float: 'right', fontSize: '1.3rem', fontWeight: 'bold' }}>
-            Send {emailReq.loading && <CircularProgress color="inherit" size={15} />}
-          </button>
+          </div>
+          <div>
+            <button className="main-search-button" onClick={() => {
+              axios.post(`https://carrentalclik.com/contact.php`, qs.stringify({
+                to: email,
+                title: `${name}: ${subject}`,
+                body: description,
+              }))
+                .then(() => {
+                  dispatchGlobalState({ type: 'success', state: 'Mail sended!' })
+                })
+            }} style={{ position: 'relative', marginTop: '1rem', borderRadius: '0.25rem', float: 'right', fontSize: '1.3rem', fontWeight: 'bold' }}>
+              Send {emailReq.loading && <CircularProgress color="inherit" size={15} />}
+            </button>
+          </div>
         </div>
       </div>
-    </div>
     </div >
   );
 }
